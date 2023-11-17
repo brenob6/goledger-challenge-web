0370fdc0659029a8f3ca8d805a122637e4bf98fb
brenob6 <brenob6@gmail.com>
2023-11-17 18:19:38 -0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
 
   return (
-		<>
-			Hello World
-		</>
+		<Grid
+			templateAreas={`"sidebar content"`}
+			gridTemplateColumns='320px 1fr'
+			gridTemplateRows='100vh'
+		>
+			<GridItem  area='sidebar'>
+			</GridItem>
+			<GridItem area='content'>
+			</GridItem>
+		</Grid>
   )
 }
 

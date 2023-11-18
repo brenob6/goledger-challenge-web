f83ada562a1e5c00cbb98d55fbefee1a6506049b
brenob6 <brenob6@gmail.com>
2023-11-17 21:21:50 -0300

    fix hooks directory

--- src/hooks/useRequest.tsx
+++ src/hooks/useRequest.tsx
@@ -1,10 +1,10 @@
 import useSWR from "swr";
-import { api } from "../../services/api";
+import { api } from "../services/api";
 
 const fetcher = (uri: string, body: any) => api.post(uri, body).then(res => res.data.result);
 
 export function useRequest(uri: string, assetType: string) {
-	return useSWR(uri, (url) => fetcher(url,{
+	return useSWR([uri, assetType], ([url, assetType]) => fetcher(url,{
 		query: {
 			selector: {
 				"@assetType": assetType

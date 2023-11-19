e9be9a3cc2a67b2594845139c30c01e1ea298501
brenob6 <brenob6@gmail.com>
2023-11-19 17:14:35 -0300

    add toast on add music

--- src/components/AddMusicButton.tsx
+++ src/components/AddMusicButton.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Menu, MenuButton, MenuGroup, MenuItem, MenuList } from "@chakra-ui/react";
+import { IconButton, Menu, MenuButton, MenuGroup, MenuItem, MenuList, useToast } from "@chakra-ui/react";
 import { IoMdAdd } from "react-icons/io";
 import { api } from "../services/api";
 import { usePlaylistContext } from "../hooks/usePlaylistContext";
@@ -9,32 +9,45 @@ interface AddMusicButtonProps {
 	title: string
 }
 
-async function handleClick(key:string, artists: string[], title:string) {
-
-	const data = await api.post("/query/readAsset", {
-		key: {
-			"@assetType": "playlist",
-			"@key": key
-		}
-	}).then((res) => res.data)
-
-	console.log(data)
-
-	let songs = data?.songs ?? [];
-	songs.push({artists, title})
-
-	api.post("/invoke/updateAsset", {
-		update: {
-			"@assetType": "playlist",
-			name: data.name,
-			songs
-		}
-	})
-}
 
 export function AddMusicButton({ artists, title }: AddMusicButtonProps) {
 
 	const { data, isLoading, error } = usePlaylistContext()
+	const toast = useToast()
+
+	async function handleClick(key:string, artists: string[], title:string) {
+		const data = await api.post("/query/readAsset", {
+			key: {
+				"@assetType": "playlist",
+				"@key": key
+			}
+		}).then((res) => res.data)
+
+		console.log(data)
+
+		let songs = data?.songs ?? [];
+		songs.push({artists, title})
+
+		api.post("/invoke/updateAsset", {
+			update: {
+				"@assetType": "playlist",
+				name: data.name,
+				songs
+			}
+		})
+		.then(() => toast({
+			title: "Sucesso",
+			description: `A música ${title} foi adicionada a playlist ${data?.name}`,
+			status: 'success',
+			duration: 3000,
+		}))
+		.catch(() => toast({
+			title: "Falha",
+			description: `Ocorreu um erro =(`,
+			status: 'error',
+			duration: 3000,
+		}))
+	}
 
 	return (
 		<Menu>

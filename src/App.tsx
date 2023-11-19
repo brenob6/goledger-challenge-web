28f5f19660c230c9ec74744ed2ffe88a9ace4a0c
brenob6 <brenob6@gmail.com>
2023-11-19 01:03:23 -0300

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import { Grid, GridItem, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
-import { Sidebar } from './components/sidebar/SidebarContainer'
-import { CardGridAlbum } from './components/cards/CardGridAlbum'
-import { CardGridSong } from './components/cards/CardGridSong'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { AlbumPage } from "./pages/AlbumPage"
+import { Layout } from "./Layout"
 
 function App() {
 
   return (
-		<Grid
-			templateAreas={`"sidebar content"`}
-			gridTemplateColumns='min-content 1fr'
-		>
-			<GridItem  area='sidebar'>
-				<Sidebar />
-			</GridItem>
-			<GridItem  area='content'>
-				<Tabs isFitted variant='enclosed'>
-					<TabList>
-						<Tab>
-							<Text as='b'>Álbuns</Text>
-						</Tab>
-						<Tab>
-							<Text as='b'>Músicas</Text>
-						</Tab>
-						<Tab>
-							<Text as='b'>Artistas</Text>
-						</Tab>
-					</TabList>
-					<TabPanels>
-						<TabPanel>
-							<CardGridAlbum />
-						</TabPanel>
-						<TabPanel>
-							<CardGridSong />
-						</TabPanel>
-					</TabPanels>
-				</Tabs>
-			</GridItem>
-		</Grid>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route path="" element={<AlbumPage />}/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
   )
 }
 
---
+++ src/Layout.tsx
@@ -0,0 +1,19 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Sidebar } from "./components/sidebar/SidebarContainer";
+import { Outlet } from "react-router-dom";
+
+export function Layout() {
+	return (
+		<Grid
+			templateAreas={`"sidebar content"`}
+			gridTemplateColumns='min-content 1fr'
+		>
+			<GridItem  area='sidebar'>
+				<Sidebar />
+			</GridItem>
+			<GridItem  area='content'>
+				<Outlet />
+			</GridItem>
+		</Grid>
+	)
+}

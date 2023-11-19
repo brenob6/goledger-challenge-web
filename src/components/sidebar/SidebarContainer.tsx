184e220e8696b94a6374db79b37912753471a00d
brenob6 <brenob6@gmail.com>
2023-11-19 09:26:31 -0300

    add home button

--- src/components/sidebar/SidebarContainer.tsx
+++ src/components/sidebar/SidebarContainer.tsx
@@ -2,10 +2,13 @@ import { Flex, HStack, Icon, Spacer, Text, useBoolean } from '@chakra-ui/react'
 import { Playlist } from './Playlist'
 import { TbLayoutSidebarLeftCollapseFilled, TbLayoutSidebarRightCollapseFilled } from "react-icons/tb"
 import { AddPlaylistButton } from './AddPlaylistButton';
+import { IoMdHome } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
 
 export function Sidebar() {
 
 	const [isCollapsed, setCollapse] = useBoolean()
+	const navigate = useNavigate()
 
 	return (
 		<Flex 
@@ -27,6 +30,20 @@ export function Sidebar() {
 			/>
 			<Text as='b' fontSize='lg' hidden={isCollapsed}> Gopher HiFi </Text>
 		</HStack>
+
+		<HStack
+			onClick={() => { navigate('/') }}
+			cursor='pointer'
+			w='auto'
+		>
+			<Icon 
+				as={IoMdHome}
+				boxSize={10}
+				ml={3}
+			/>
+			<Text as='b' fontSize='lg' hidden={isCollapsed}> Home </Text>
+		</HStack>
+
 		<Playlist isCollapsed={isCollapsed} />
 		<Spacer />
 		<AddPlaylistButton isCollapsed={isCollapsed}/>

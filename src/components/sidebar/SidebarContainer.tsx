dec22dd7b61cf693a3eca6675835b5cff8649d05
brenob6 <brenob6@gmail.com>
2023-11-19 21:54:29 -0300

    refactor CreatePlaylistButton

--- src/components/sidebar/SidebarContainer.tsx
+++ src/components/sidebar/SidebarContainer.tsx
@@ -1,9 +1,9 @@
 import { Flex, HStack, Icon, Spacer, Text, useBoolean } from '@chakra-ui/react'
 import { Playlist } from './Playlist'
 import { TbLayoutSidebarLeftCollapseFilled, TbLayoutSidebarRightCollapseFilled } from "react-icons/tb"
-import { AddPlaylistButton } from './AddPlaylistButton';
 import { IoMdHome } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
+import { CreatePlaylistButton } from './CreatePlaylistButton';
 
 export function Sidebar() {
 
@@ -42,12 +42,12 @@ export function Sidebar() {
 				boxSize={10}
 				ml={3}
 			/>
-			<Text as='b' fontSize='lg' hidden={isCollapsed}> Home </Text>
+			<Text as='b' fontSize='lg' hidden={isCollapsed}>Todas as Músicas </Text>
 		</HStack>
 
 		<Playlist isCollapsed={isCollapsed} />
 		<Spacer />
-		<AddPlaylistButton isCollapsed={isCollapsed}/>
+		<CreatePlaylistButton isCollapsed={isCollapsed}/>
 		</Flex>
 	);
 }
